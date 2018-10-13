cd0f70fc10bccd61593834abcbc8417eb6ed6850
Geert-Jan Kruijff <gj.kruijff@gmail.com>
2018-10-13 14:47:36 +0200

    wxPython example of frame and menubar

---
+++ code/wxhelloworld.py
@@ -0,0 +1,56 @@
+
+
+import wx
+
+class HelloFrame(wx.Frame):
+
+    def __init__ (sefl, *args, **kw):
+        super(HelloFrame, self).__init__(*args, **kw)
+        pnl = wx.Panel(self)
+
+        st = wx.StaticText(pnl, label="Hello World", pos=(25,25))
+        font = st.GetFont()
+        font.PointSize += 10
+        font = font.Bold()
+        st.SetFont(font)
+
+        self.makeMenuBar()
+
+        self.CreateStatusBar()
+        self.SetStatusText("Welcome to wxPython!")
+
+    def makeMenuBar(self):
+        fileMenu = wx.Menu()
+        helloItem = fileMenu.append(-1, "&Hello...\tCtrl-H", "Help string shown in status bar for this menu item")
+        fileMenu.AppendSeparator()
+        exitItem = fileMenu.Append(wx.ID_EXIT)
+
+        helpMenu = wx.Menu()
+        aboutItem = helpMenu.Append(wx.ID_ABOUT)
+
+        menuBar = wx.MenuBar()
+        menuBar.Append(fileMenu, "&File")
+        menuBar.Append(helpMenu, "&Help")
+
+        self.SetMenuBar(menuBar)
+
+        self.Bind(wx.EVT_MENU, self.OnHello, helloItem)
+        self.Bind(wx.EVT_MENU, self.OnExit, exitItem)
+        self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
+
+    def OnExit(self, event):
+        self.Close(True)
+
+    def OnHello(self, event):
+        wx.MessageBox("Hello again")
+
+    def OnAbout(self, event):
+        wx.MessageBox("This is a Hello World sample", "About Hello World", wx.OK | wx.ICON_INFORMATION)
+
+
+if __name__ == '__main__':
+    ## When this module is run (not imported) then create the app, the frame, show it, start event loop
+    app = wx.App()
+    frm = HelloFrame(None, title = "Hello World Again")
+    frm.Show()
+    app.MainLoop()
